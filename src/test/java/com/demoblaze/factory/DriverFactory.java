a422a4f4e33a0c84dfbc1bc9767d3257468e751d
MohammedNimer <mohammed_nimer_98@yahoo.com>
2023-11-05 18:50:35 +0300

    Update the project

--- src/test/java/com/demoblaze/factory/DriverFactory.java
+++ src/test/java/com/demoblaze/factory/DriverFactory.java
@@ -3,6 +3,7 @@ package com.demoblaze.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -10,7 +11,9 @@ public class DriverFactory {
 
     public WebDriver initializeDriver(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver=new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         return driver;
